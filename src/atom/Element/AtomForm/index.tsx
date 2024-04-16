048f14e18c17c8b8474408f2547203171ef9b67c
MarcosRehtanz <marcos.rehtanz@gmail.com>
2024-04-16 03:02:31 -0300

    added three elements

---
+++ src/atom/Element/AtomForm/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+type AtomFormProps = React.FormHTMLAttributes<HTMLFormElement>;
+
+export interface AtomFormElement extends React.JSX.Element {}
+
+export const AtomForm = ({ className, ...rest }: AtomFormProps) =>
+  (
+    <form className={`KUI-atom-element-form ${className || ''}`} {...rest} />
+  ) as AtomFormElement;
