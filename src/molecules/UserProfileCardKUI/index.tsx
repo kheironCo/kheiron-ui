5364dd50654e4eb7b1479ce60e536945a4e0bf19
jalejotorresm <alejandrotorres5286@gmail.com>
2024-04-29 16:40:51 -0500

    "KHUI-4 Boilerplate creado"

---
+++ src/molecules/UserProfileCardKUI/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export type UserProfileCardKUIProps = {
+  image?: string;
+  head: string;
+  body: string;
+};
+
+export const UserProfileCardKUI = ({
+  image,
+  head,
+  body,
+}: UserProfileCardKUIProps) => <></>;
