0e79b6ee8e64e6a11d8603ad014fd981edc54cd7
jalejotorresm <alejandrotorres5286@gmail.com>
2024-05-05 07:57:24 -0500

    KHUI-13 Elemento completado

--- src/element/AtomTd/index.tsx
+++ src/element/AtomTd/index.tsx
@@ -4,7 +4,7 @@ export interface AtomTdElement extends React.JSX.Element {}
 
 export const AtomTd = ({ children, className, ...rest }: AtomTdProps) =>
   (
-    <td className={`KUI-atom-element-Td ${className || ''}`} {...rest}>
+    <td className={`KUI-atom-element-td ${className || ''}`} {...rest}>
       {children}
     </td>
   ) as AtomTdElement;
