791e82b2cdfad71945ac58fcadb2f13413fb6f54
Gaston Jaurena <gassjau@gmail.com>
2024-05-05 16:33:37 -0300

    cambios en Search

--- src/molecules/SearchBarKUI/index.tsx
+++ src/molecules/SearchBarKUI/index.tsx
@@ -1,23 +1,31 @@
-
-import { css } from '@emotion/react';
 import { AtomSearch, AtomButton, AtomInput, AtomForm } from '../../element';
 import { button, input } from './styles';
-import { ChangeEvent, FormEvent } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 
 export type SearchBarKUIProps = {
-  value: string;
   handleChange?: (e: ChangeEvent<HTMLInputElement>) => void;
   handleSearch?: (e: FormEvent<HTMLFormElement>) => void;
+  getValue?: (value: string) => void;
 };
 
-export const SearchBarKUI = ({ value, handleChange, handleSearch }: SearchBarKUIProps) => {
+export const SearchBarKUI = ({ handleSearch, getValue }: SearchBarKUIProps) => {
+
+  const [_value, _setValue] = useState("");
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (getValue) getValue(e.target.value);
+    _setValue(e.target.value);
+  };
+  const _onSubmit = (e: FormEvent<HTMLFormElement>)=>{
+    // si existe la función se ejecutará
+    if(handleSearch) handleSearch(e);
+  }
 
   return(
     <AtomSearch className='KUI-search-bar-search KUI-search-bar-root'>
-      <AtomForm className='KUI-search-bar-form' onSubmit={handleSearch}>
+      <AtomForm className='KUI-search-bar-form' onSubmit={_onSubmit}>
         <AtomButton className='KUI-search-bar-button' css={button} type="submit">Search</AtomButton>
-        <AtomInput className='KUI-search-bar-input' css={input} onChange={handleChange} value={value} />
+        <AtomInput className='KUI-search-bar-input' css={input} onChange={handleChange} value={_value} />
       </AtomForm>
     </AtomSearch>
   );
