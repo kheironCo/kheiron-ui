1a08c51d58e34b4e718aad1f6cee3a72af2cb4a2
Rennerx3 <rmokosce21@hotmail.com>
2024-05-21 17:09:38 -0300

    inputtextarea

---
+++ src/molecules/InputFieldTextareaKUI/index.tsx
@@ -0,0 +1,17 @@
+import { LabelKUI, LabelKUIProps } from "molecules/LabelKUI";
+import { AtomTextArea } from "@KUI-element";
+
+export const InputFieldTextareaKUI = ({label, ...rest}: LabelKUIProps) => {
+
+    return (
+        <>
+            <LabelKUI
+                label={label}
+            >
+                <AtomTextArea
+                    {...rest}
+                />
+            </LabelKUI>
+        </>
+    );
+};
