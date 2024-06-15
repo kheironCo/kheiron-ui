61e9a6e3ada1ab43ed6524decf25122b0748d20d
jalejotorresm <alejandrotorres5286@gmail.com>
2024-06-15 16:03:27 -0500

    KHUI-36 Componente NotificationKUI creado y optimizado. example/index mejorado para React moderno

--- src/molecules/NotificationKUI/index.tsx
+++ src/molecules/NotificationKUI/index.tsx
@@ -1,5 +1,12 @@
 import { AtomDiv, AtomDivProps, AtomParagraph, AtomSection } from '../../element';
-import { InfoIcon, WarnIcon, ErrorIcon, SuccessIcon } from './iconStyles';
+import { FiAlertTriangle, FiCheckSquare, FiInfo, FiXOctagon } from 'react-icons/fi';
+import {
+  WarnNotificationRoot,
+  SuccessNotificationRoot,
+  InfoNotificationRoot,
+  ErrorNotificationRoot,
+  NotificationBody,
+} from './styles';
 
 export type NotificationType = 'warn' | 'success' | 'info' | 'error';
 
@@ -10,40 +17,48 @@ export type NotificationKUIProps = AtomDivProps & {
 
 /*
 Lógica para asignar icono y clase en base al NotificationType
-
 Se que hay una manera de unificar estas dos funciones para que los strings de la clase y del icono retornen en una sola estructura, pero para tener el componente listo de forma mas rápida prefiero separarlos. Cualquier sugerencia es bien recibida.
 */
 const icon = (type: NotificationType) => {
+  const warnIcon = { height: '2rem', color: 'yellow' };
+  const successIcon = { height: '2rem', color: 'green' };
+  const infoIcon = { height: '2rem', color: 'blue' };
+  const errorIcon = { height: '2rem', color: 'red' };
+
   switch (type) {
     case 'warn':
-      return <WarnIcon />;
+      return <FiAlertTriangle style={warnIcon} />;
     case 'success':
-      return <SuccessIcon />;
+      return <FiCheckSquare style={successIcon} />;
     case 'info':
-      return <InfoIcon />;
+      return <FiInfo style={infoIcon} />;
     case 'error':
-      return <ErrorIcon />;
+      return <FiXOctagon style={errorIcon} />;
+    default:
   }
 };
 
-/*
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const notificationClass = (type: NotificationType) => {
   switch (type) {
     case 'warn':
+      return WarnNotificationRoot;
     case 'success':
+      return SuccessNotificationRoot;
     case 'info':
+      return InfoNotificationRoot;
     case 'error':
+      return ErrorNotificationRoot;
   }
 };
-*/
 
 export const NotificationKUI = ({ type, description, ...rest }: NotificationKUIProps) => (
   <AtomDiv {...rest}>
-    <AtomSection className="KUI-user-profile-card-root">
+    <AtomSection css={notificationClass(type)} className="KUI-user-profile-card-root">
       <AtomDiv>{icon(type)}</AtomDiv>
       <AtomDiv>
-        <AtomParagraph className="KUI-user-profile-card-body">{description}</AtomParagraph>
+        <AtomParagraph css={NotificationBody} className="KUI-user-profile-card-body">
+          {description}
+        </AtomParagraph>
       </AtomDiv>
     </AtomSection>
   </AtomDiv>
--- src/molecules/NotificationKUI/styles.tsx
+++ src/molecules/NotificationKUI/styles.tsx
@@ -1,10 +1,53 @@
 import { css } from '@emotion/react';
 
-export const NotificationRoot = css`
+export const WarnNotificationRoot = css`
   width: fit-content;
   height: auto;
   padding: 0.25rem 1rem 0.25rem 0.5rem;
-  background-color: #4441;
+  background-color: #ffffe0;
+  border: solid 1px yellow;
+  border-radius: 0.5rem;
+  box-sizing: border-box;
+  display: flex;
+  gap: 1rem;
+  justify-content: left;
+  align-items: center;
+`;
+
+export const SuccessNotificationRoot = css`
+  width: fit-content;
+  height: auto;
+  padding: 0.25rem 1rem 0.25rem 0.5rem;
+  background-color: #90ee90;
+  border: solid 1px green;
+  border-radius: 0.5rem;
+  box-sizing: border-box;
+  display: flex;
+  gap: 1rem;
+  justify-content: left;
+  align-items: center;
+`;
+
+export const InfoNotificationRoot = css`
+  width: fit-content;
+  height: auto;
+  padding: 0.25rem 1rem 0.25rem 0.5rem;
+  background-color: #add8e6;
+  border: solid 1px blue;
+  border-radius: 0.5rem;
+  box-sizing: border-box;
+  display: flex;
+  gap: 1rem;
+  justify-content: left;
+  align-items: center;
+`;
+
+export const ErrorNotificationRoot = css`
+  width: fit-content;
+  height: auto;
+  padding: 0.25rem 1rem 0.25rem 0.5rem;
+  background-color: #ffb6c1;
+  border: solid 1px red;
   border-radius: 0.5rem;
   box-sizing: border-box;
   display: flex;
