2e749f51c935788a0ad8af8040f22384d24a43ff
MarcosRehtanz <marcos.rehtanz@gmail.com>
2024-06-30 23:14:24 -0300

    fix label

--- src/element/Heading/index.tsx
+++ src/element/Heading/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import { HeadingStyle } from './styles';
+import { HeadingStyle, HeadingStyleProps } from './styles';
 
 type VariantType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
-type AtomHeadingProps = React.HTMLAttributes<HTMLHeadingElement> & {
-  variant?: VariantType;
-  as?: VariantType;
-};
+type AtomHeadingProps = React.HTMLAttributes<HTMLHeadingElement> &
+  HeadingStyleProps & {
+    variant?: VariantType;
+    as?: VariantType;
+  };
 
 export interface AtomHeadingElement extends React.JSX.Element {}
 
-export const Heading = ({ variant = 'h3', as, children, className, ...rest }: AtomHeadingProps) => {
+export const Heading = ({
+  variant = 'h3',
+  as,
+  children,
+  className,
+  weight,
+  ...rest
+}: AtomHeadingProps) => {
   const HeadingTag = as ?? variant;
   return (
     <HeadingTag
-      css={HeadingStyle}
+      css={HeadingStyle({ weight })}
       className={`KUI-atom-element-heading-${variant} ${className || ''}`}
       {...rest}
     >
