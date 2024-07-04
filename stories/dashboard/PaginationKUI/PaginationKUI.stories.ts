ee90f2cef7190956cef129045820f605d21070b4
MarcosRehtanz <marcos.rehtanz@gmail.com>
2024-07-04 09:33:30 -0300

    feat: add description in storybook

--- stories/dashboard/PaginationKUI/PaginationKUI.stories.ts
+++ stories/dashboard/PaginationKUI/PaginationKUI.stories.ts
@@ -4,6 +4,17 @@ import { PaginationKUI } from 'dashboard';
 const meta: Meta<typeof PaginationKUI> = {
   title: 'dashboard/Pagination',
   component: PaginationKUI,
+  argTypes: {
+    neighbors: {
+      description: 'This param receive the number of adjacent pages',
+      type: 'number',
+      control: 'number',
+    },
+    totalPages: {
+      control: 'number',
+      type: 'number',
+    },
+  },
 };
 export default meta;
 
