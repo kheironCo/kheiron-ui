c185570dc32db12845b163efe5ca6675184e53ea
Gaston Jaurena <gassjau@gmail.com>
2024-07-10 14:47:54 -0300

    Change modal to styledComponents

---
+++ src/utily/ModalKUI/styles.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { AtomDialog, AtomDiv } from '../../element';
+import ROOT from '../../common/root';
+
+export const AtomDialogComponentStyled = styled(AtomDialog)`
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  border: none;
+  display: none;
+
+  & > .KUI-modal-content {
+    display: none;
+  }
+
+  &[open] {
+    background-color: #0000004d;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+
+    & > .KUI-modal-content {
+      display: block;
+    }
+  }
+`;
+
+export const AtomDivComponentStyled = styled(AtomDiv)`
+        padding: '16px',
+        borderRadius: '8px',
+        backgroundColor: ${ROOT.color.gray[200]},
+`;
