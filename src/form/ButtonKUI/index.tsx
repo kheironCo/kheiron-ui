7cc283575c0a18925b0fb251335b54f56e56a220
MarcosRehtanz <marcos.rehtanz@gmail.com>
2024-08-15 06:10:56 -0300

    fix buttonKUI

--- src/form/ButtonKUI/index.tsx
+++ src/form/ButtonKUI/index.tsx
@@ -3,7 +3,7 @@ import { ButtonPalette, ButtonVariant } from './type';
 
 import { ReactNode } from 'react';
 
-export type ButtonKUIProps = {
+export type ButtonKUIProps = Parameters<typeof ButtonStyled>[0] & {
   className?: string;
   variant?: ButtonVariant;
   palette?: ButtonPalette;
@@ -17,10 +17,17 @@ export const ButtonKUI = ({
   label,
   variant = 'filled',
   palette = 'primary',
+  disabled = false,
   ...rest
 }: ButtonKUIProps) => {
   return (
-    <ButtonStyled variant={variant} palette={palette} className={`KUI-button ${className || ''}`} {...rest}>
+    <ButtonStyled
+      variant={variant}
+      palette={palette}
+      className={`KUI-button ${className || ''}`}
+      disabled={disabled}
+      {...rest}
+    >
       {label ?? children ?? ''}
     </ButtonStyled>
   );
